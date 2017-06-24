7e02ca7f85fec36129b6b631cc60b70913adcfc8
YY <550872569@qq.com>
2017-06-24 22:30:38 +0800

    add readme file and commit production

---
+++ BookMusicMoviePlayer/BookMusicMoviePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BookMusicMoviePlayer
+//
+//  Created by Yan on 2017/6/24.
+//  Copyright © 2017年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BookMusicMoviePlayer
 BookMusicMoviePlayer
+
+i will create a player for read book,listen music and look movie.
+# by yan 2017.06.24 22:29 PM #
